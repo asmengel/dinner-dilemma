e8f132eef24a7974f807ec0341c9a18aebc0996a
L Barbee <bluelb84@gmail.com>
2017-07-27 15:18:55 -0400

    added functionality to each submit button.  App routes to different page views.

--- dinner.js
+++ dinner.js
@@ -1,48 +1,69 @@
+const STATE = {
+  JSONresults: [],
+  route: 'start' || 'results' || 'recipe',
+};
 
 
+const PAGE_ELEMENTS = {
+  'start': $('.js-home-page'),
+  'results': $('.js-results-page'),
+  'recipe': $('.js-recipe-page'),
+};
 
 
+function renderApp(state, elements) {
+  // default to hiding all routes, then show the current route
+  Object.keys(elements).forEach(function(route) {
+    elements[route].hide();
+  });
+  elements[state.route].show();
 
-// $(function () {
-//   $(STARTBUTTON).css('border', '10px solid red').click(function (event) {
-//     event.preventDefault();
-//     STATE.route = 'question'
-//     renderQuiz(STATE, ELEMENTS);
-//   });
-//   $(QUESTION).css('border', '10px solid blue').submit(function (event) {
-//     event.preventDefault();
-
-//     STATE.route = 'feedbackpage'
-//     renderQuiz(STATE, ELEMENTS);
-//   });
-//   $(NEXTBUTTON).css('border', '10px solid green').click(function (event) {
-//     event.preventDefault();
-//     currentQuestion++
-//     //--------------------------------------------------------------------------------REPLACE CONDITIONAL CHECK WITH RUNNING CHOOSE QUESTION FUNCTION 
-//     chooseQuestion();
-
-//     renderQuiz(STATE, ELEMENTS);
-//   });
-//   $(TRYAGAINBUTTON).css('border', '10px solid yellow').click(function (event) {
-//     event.preventDefault();
-//     STATE.route = 'startpage'
-//     renderQuiz(STATE, ELEMENTS);
-//   });
-// });
+  if (state.route === 'start') {
+    console.log('render Start Page');
+    //renderStartPage(state, elements[state.route]);
+  }
+  else if (state.route === 'results') {
+    console.log('render Results Page');
+    renderResultsPage(state, elements[state.route]);
+  }
+  else if (state.route === 'recipe') {
+    console.log('render Recipe Page');
+    renderRecipePage(state, elements[state.route]);
+  }
+}
 
 
-// function renderQuiz(state, elements) {
-//   $(elements.startpage).hide();
-//   $(elements.feedbackpage).hide();
-//   $(elements.scorepage).hide();
-//   $(elements.question).hide();
-//   // console.log('route', state.route)
-//   // console.log('selector', elements[state.route])
-//   // console.log('jquery', $(elements[state.route]))
-//   if (state.route === 'question') {
-//     console.log(STORE[currentQuestion]);
-//     $(QUESTIONCONTAINER).find('p').text(STORE[currentQuestion].question);
-//   }
-//   $(elements[state.route]).show()
+$('.js-home-submit').on('click', function(event) {
+  event.preventDefault();
+  console.log('home submit button clicked');
+  STATE.route = 'results';
+  renderApp(STATE, PAGE_ELEMENTS); 
+});
 
-// }
+
+function renderResultsPage(state, element){
+    console.log('You are on the results page');
+}
+
+
+// User submits new search
+$('.js-results-submit').click(function(event){
+  event.preventDefault();
+    STATE.route = 'results';
+  console.log('you are back on the results page with new results');
+  renderApp(STATE, PAGE_ELEMENTS);
+});
+
+
+// Returns user to home screen
+$('.js-return-submit').click(function(event){
+  event.preventDefault();
+    STATE.route = 'start';
+  console.log('you are back HOME');
+  renderApp(STATE, PAGE_ELEMENTS);
+});
+
+
+$(document).ready(function() {
+  renderApp(STATE, PAGE_ELEMENTS);
+});
